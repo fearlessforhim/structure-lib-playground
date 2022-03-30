6488e0e09f6e600c6d33b0192d0b1bb4635f557a
Jonathan Cameron <jonathancameron6@protonmail.com>
2022-03-30 16:26:26 -0700

    Add basic theme switcher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, {useState} from 'react';
 import './App.scss';
 import {Button} from 'structure-lib'
 
 function App() {
+
+  let [useDark, setUseDark] = useState(false);
+
   return (
-    <div className="app-root">
-      <div className={`page-wrap`}></div>
+    <div className={`app-root ${useDark ? `dark` : ``}`}>
+      <div className={`page-wrap`}>
+        <span
+          onClick={() => {setUseDark(false);}}
+        >
+          Light
+        </span>
+        <span
+          onClick={() => {setUseDark(true);}}
+        >
+          Dark
+        </span>
+      </div>
     </div>
   );
 }
