7740a7879315c41adf8b5338066c9aca99e906fb
Jonathan Cameron <jonathancameron6@protonmail.com>
2022-03-30 16:21:20 -0700

    Enable light and dark themes in styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
 import {Button} from 'structure-lib'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-root">
+      <div className={`page-wrap`}></div>
     </div>
   );
 }
